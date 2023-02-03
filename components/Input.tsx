0c90aba336691ee11612cff39ddbc13ebae7f81c
MarcinParda <marcin98parda@gmail.com>
2023-02-03 19:56:11 +0100

    feat: :sparkles: added submit button for question

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,14 +6,16 @@ interface InputProps {
   name?: string;
   placeholder?: string;
   disabled?: boolean;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Input = ({ label, ...inputParams }: InputProps) => {
   return (
-    <label className="mb-3 block text-base font-medium text-black">
+    <label className="block text-base font-medium text-black">
       <span>{label}</span>
       <input
-        className="mt-1 border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+        className="mt-1 border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
         {...inputParams}
       />
     </label>
