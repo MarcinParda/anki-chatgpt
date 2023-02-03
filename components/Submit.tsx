9006aaf9606eb10f6c835ac4b56df27cc75cba2a
MarcinParda <marcin98parda@gmail.com>
2023-02-03 21:44:10 +0100

    feat: :sparkles: sending simple answer request to API

--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 interface SubmitProps {
-  label?: string;
+  label: string;
+  onSubmit: () => void;
   icon?: React.ReactNode;
   iconPosition?: 'left' | 'right';
 }
 
 export const Submit = ({
-  icon,
   label,
+  onSubmit,
+  icon,
   iconPosition = 'right',
 }: SubmitProps) => {
   return (
     <button
       type="submit"
       className="w-full bg-primary inline-flex items-center justify-center rounded-md px-4 py-[13px] text-white hover:bg-opacity-90"
+      onClick={onSubmit}
     >
       {icon && iconPosition === 'left' ? (
         <span className="mr-3">{icon}</span>
