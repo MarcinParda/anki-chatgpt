6bffa9c63922c3c55bc063483e520e7b219951a4
MarcinParda <marcin98parda@gmail.com>
2023-02-04 16:53:58 +0100

    feat: add react-query and use it for create-card page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
