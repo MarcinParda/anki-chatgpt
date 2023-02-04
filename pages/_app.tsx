f8430e860e67fe68cd1535fc5185d5e715962fa3
MarcinParda <marcin98parda@gmail.com>
2023-02-04 19:47:33 +0100

    feat: redirect to create-card page initially

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname === '/') {
+      router.push('/create-card');
+    }
+  }, [router.pathname]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
